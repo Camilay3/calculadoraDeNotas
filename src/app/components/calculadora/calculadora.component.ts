ffcca0b0a61f3358a35aead1101ab7042893e3c7
Camila Santiago <camila.santiago026@gmail.com>
2026-01-11 09:11:32 -0300

    feat: Cálculo da AF

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormsModule, } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormsModule, FormArray, } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 import { TuiButton, TuiLabel, TuiTextfield, TuiTitle, TuiError, TuiHint } from '@taiga-ui/core';
-import { TuiInputNumber, TuiRadio, TuiRadioList } from '@taiga-ui/kit';
+import { TuiCheckbox, TuiInputNumber, TuiRadio, TuiRadioList } from '@taiga-ui/kit';
 import { TuiForm, TuiHeader } from '@taiga-ui/layout';
 
 
@@ -25,21 +25,41 @@ import { TuiForm, TuiHeader } from '@taiga-ui/layout';
 		TuiRadio,
 		TuiRadioList,
 		RouterLink,
+		TuiCheckbox,
 	],
 	templateUrl: './calculadora.component.html',
 	// styleUrl: './calculadora.component.scss',
 })
 export class CalculadoraComponent {
 	constructor() {
-		this.notaForm.controls.notaEsperada.valueChanges.subscribe(() => {
-			this.updateValidators();
-		});
+		this.notaForm.controls.notaEsperada.valueChanges.subscribe(() => { this.updateValidators(); });
 		this.updateValidators();
 	}
 
 	mediaN1: number | null = null;
 	nota: number | null = null;
+	isAF: boolean = false;
+	notaAF: number | null = null;
+	mediaSimulada: number | null = null;
 	value: string | null = null;
+
+	protected readonly choices = [
+		{
+			name: 'Quarta prova',
+			description: 'Calcula quanto você precisará tirar na última prova. Será necessário fornecer a terceira nota',
+		},
+		{
+			name: 'N2',
+			description: 'Calcula qual média você precisará na N2',
+		},
+	];
+
+	protected readonly points = [
+		'Um ponto na menor nota da N1',
+		'Um ponto na menor nota da N2',
+		'Um ponto na média da N1',
+		'Um ponto na média da N2'
+	];
 
 	readonly notaForm = new FormGroup({
 		primeiraNota: new FormControl<number | null>(null, [
@@ -57,18 +77,45 @@ export class CalculadoraComponent {
 			Validators.required
 		),
 
-		terceiraNota: new FormControl<number | null>(null, [Validators.min(0), Validators.max(10)]),
+		terceiraNota: new FormControl<number | null>(null, [
+			Validators.min(0),
+			Validators.max(10)
+		]),
+
+		points: new FormArray(
+			this.points.map(() => new FormControl(false))
+		),
 	});
+
+	get pointsArray(): FormArray<FormControl<boolean>> {
+		return this.notaForm.controls.points as FormArray<FormControl<boolean>>;
+	}
+
+	get pontosSelecionados(): string[] {
+		return this.pointsArray.value
+		.map((checked, i) => checked ? this.points[i] : null)
+		.filter((v): v is string => v !== null);
+	}
 
 	calcularNota(): void {
 		const primeiraNota = this.notaForm.value.primeiraNota!;
 		const segundaNota = this.notaForm.value.segundaNota!;
 		const terceiraNota = this.notaForm.value.terceiraNota;
+		const pontos = this.pontosSelecionados;
+
+		console.log(pontos)
 
 		this.mediaN1 = (primeiraNota + segundaNota) / 2;
 		let formulaMedia = (35 - (primeiraNota + segundaNota)) / 3;
 
-		this.nota = terceiraNota ? formulaMedia * 2 - terceiraNota : formulaMedia;
+		const hasTerceira = terceiraNota !== null && terceiraNota !== undefined;
+		this.nota = hasTerceira ? formulaMedia * 2 - terceiraNota : formulaMedia;
+
+		if (hasTerceira && this.nota > 10) {
+			this.mediaSimulada = (((this.mediaN1 * 2) + (((terceiraNota + 10) / 2) * 3)) / 5);
+			this.isAF = this.mediaSimulada >= 3;
+			if (this.isAF) this.notaAF = 10 - this.mediaSimulada;
+		}
 	}
 
 	getErroTratado(formControlName: string): string | null {
@@ -81,18 +128,6 @@ export class CalculadoraComponent {
 
 		return null;
 	}
-
-	protected readonly objects = [
-		{
-			name: 'Quarta prova',
-			description:
-				'Calcula quanto você precisará tirar na última prova. Será necessário fornecer a terceira nota',
-		},
-		{
-			name: 'N2',
-			description: 'Calcula qual média você precisará na N2',
-		},
-	];
 
 	get isQuartaProvaSelected(): boolean {
 		const selectedOption = this.notaForm.controls.notaEsperada.value;
