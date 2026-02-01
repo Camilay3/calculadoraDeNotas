6d59fc031b70a840772a906974eec14268a08b74
Camila Santiago <camila.santiago026@gmail.com>
2026-02-01 12:29:08 -0300

    fix: Problemas na execução dos testes

--- src/app/components/calculadora/calculadora.component.spec.ts
+++ src/app/components/calculadora/calculadora.component.spec.ts
@@ -1,23 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalculadoraComponent } from './calculadora.component';
 
 describe('CalculadoraComponent', () => {
-  let component: CalculadoraComponent;
-  let fixture: ComponentFixture<CalculadoraComponent>;
+	let component: CalculadoraComponent;
+	let fixture: ComponentFixture<CalculadoraComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CalculadoraComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [CalculadoraComponent]
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(CalculadoraComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(CalculadoraComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	// Testes para as linhas 94-116,121-223,233-237,248-250,256-257
 });
