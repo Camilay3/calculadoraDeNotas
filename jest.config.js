6f86057c4d2d74830e78294bb3de352e66ae9996
Camila Santiago <camila.santiago026@gmail.com>
2026-02-01 17:46:57 -0300

    ci: Github Actions para testes e deploy

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
+	preset: 'jest-preset-angular',
+	globalSetup: 'jest-preset-angular/global-setup',
+
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+
+	coverageThreshold: {
+	global: {
+		statements: 80,
+		branches: 80,
+		functions: 80,
+		lines: 80
+	}
+	}
 };
